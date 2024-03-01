34284e086c7cccb8610b78acc6ecb1754e7c193e
bit-wise <bit-wise@users.noreply.github.com>
2024-03-01 07:53:36 -0500

    Refactored repel and attract methods in Particle class

--- Particle/sketch.js
+++ Particle/sketch.js
@@ -20,56 +20,6 @@ class Particle {
         this.maxForce = 0.2;
     }
 
-    // repel should be stronger at close distances and weaker at far distances
-    // attract should be stronger at far distances and weaker at close distances
-    repel(particles) {
-        let repulsion = this.repulsion;
-        let repulsionForce = createVector();
-        particles.forEach(particle => {
-            if (particle === this) return;
-            // get the minimum distance between the two particles and compensate for edge wrapping
-            let d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
-            if (d > 50 && d < 100) {
-                let force = p5.Vector.sub(this.pos, particle.pos);
-                force.setMag(repulsion / d);
-                repulsionForce.add(force);
-            } else
-            if (d < 10) {
-                let force = p5.Vector.sub(this.pos, particle.pos);
-                force.setMag(0.5);
-                repulsionForce.add(force);
-            }
-            this.acc.add(repulsionForce);
-        });
-    }
-
-    attract(particles) {
-        let attraction = this.attraction;
-        let attractionForce = createVector();
-        particles.forEach(particle => {
-            if (particle === this) return;
-            let d;
-            // d = min(
-            //     dist(this.pos.x, this.pos.y, particle.pos.x - width, particle.pos.y - height),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x - width, particle.pos.y),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x - width, particle.pos.y + height),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x + width, particle.pos.y - height),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x + width, particle.pos.y),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x + width, particle.pos.y + height),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y - height),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y),
-            //     dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y + height)
-            // );
-            d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
-            if (d > 20 && d < 50) {
-                let force = p5.Vector.sub(particle.pos, this.pos);
-                force.setMag(attraction / d);
-                attractionForce.add(force);
-            }
-        });
-        this.acc.add(attractionForce);
-    }
-
     update() {
         this.vel.add(this.acc);
         this.vel.limit(this.maxSpeed);
@@ -91,16 +41,83 @@ class Particle {
     }
 
     show() {
-        stroke(this.color.r, this.color.g, this.color.b);
+        stroke(colors[this.color].r, colors[this.color].g, colors[this.color].b);
         strokeWeight(4);
         point(this.pos.x, this.pos.y);
     }
+
+    distance(a, b) {
+        if(Math.abs(a.x - b.x) > W2) {
+            b.x -= W;
+        }
+        if(Math.abs(a.y - b.y) > H2) {
+            b.y -= H;
+        }
+        // if (b.x > W * 0.5) {
+        //     b.x -= W;
+        // }
+        // if (b.x < W * -0.5) {
+        //     b.x += W;
+        // }
+        // if (b.y > H * 0.5) {
+        //     b.y -= H;
+        // }
+        // if (b.y < H * -0.5) {
+        //     b.y += H;
+        // }
+        return dist(a.x, a.y, b.x, b.y);
+    }
+
+    // repel should be stronger at close distances and weaker at far distances and should accommodate for edge wrapping
+    repel(particles) {
+        let steering = createVector();
+        for (let other of particles) {
+            let d = this.distance(this.pos, other.pos);
+            if (other != this && d < this.repulsion && d > 0) {
+                let diff = p5.Vector.sub(this.pos, other.pos);
+                diff.div(d);
+                steering.add(diff);
+            }
+        }
+        this.acc.add(steering);
+    }
+
+    // attract should be stronger at far distances and weaker at close distances and should accommodate for edge wrapping
+    attract(particles) {
+        let steering = createVector();
+        for (let other of particles) {
+            let d = this.distance(this.pos, other.pos);
+            if (other != this && d < this.attraction && d > 10) {
+                let diff = p5.Vector.sub(this.pos, other.pos);
+                diff.div(d);
+                steering.sub(diff);
+            }
+        }
+        this.acc.add(steering);
+    }
+
+    // align should steer towards the average heading of all particles
+    align(particles) {
+        let steering = createVector();
+        for (let other of particles) {
+            if (other != this) {
+                steering.add(other.vel);
+            }
+        }
+        steering.div(particles.length);
+        steering.sub(this.vel);
+        this.acc.add(steering);
+    }
+
+
 }
 
 // Path: Particle/sketch.js
 
 const W = 600;
 const H = 600;
+const W2 = W / 2;
+const H2 = H / 2;
 const totalParticles = 1000;
 let swarms = [];
 let numSwarms = 5;
@@ -120,10 +137,10 @@ for (let i = 0; i < numSwarms; i++) {
 function setup() {
     createCanvas(W, H);
     for (let i = 0; i < numSwarms; i++) {
-        let swarm = {particles: []};
-        let color = colors[i];
-        let attraction = random(W/3);
-        let repulsion = random(W/3);
+        let swarm = { particles: [] };
+        let color = i;
+        let attraction = random(W / 4);
+        let repulsion = random(W / 4);
         for (let j = 0; j < numParticles; j++) {
             let x = random(width);
             let y = random(height);
@@ -136,13 +153,14 @@ function setup() {
 }
 
 function draw() {
-    background(0,0,0,16);
+    background(0, 0, 0, 16);
     for (let i = 0; i < swarms.length; i++) {
         let swarm = swarms[i];
         let particles = swarm.particles;
         particles.forEach(particle => {
             particle.repel(particles);
-            // particle.attract(particles);
+            particle.attract(particles);
+            // particle.align(particles);
             particle.update();
             particle.edges();
         });
